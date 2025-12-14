0383a00dcbbdec0a6afa0f224f033fd77ff6cf75
Samuele-Finotello <samuelefinotello.147@gmail.com>
2025-12-14 17:45:49 +0100

    [add] feat end milestone 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
 
 function App() {
 
+  const products = [
+    { name: 'Mela', price: 0.5 },
+    { name: 'Pane', price: 1.2 },
+    { name: 'Latte', price: 1.0 },
+    { name: 'Pasta', price: 0.7 },
+  ];
+
   return (
     <>
-
+      <div>
+        <ul>
+          {products.map(product => {
+            return (
+              <li>
+                <span className="me-15">{product.name}</span>
+                <span>€ {product.price}</span>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
     </>
   )
 }
